a70728e617c5adaf26c803e354c16064567c77cc
zahra imani <zahra.imani753@gmail.com>
2024-05-22 19:55:20 +0330

    header and footer

---
+++ assests/js/main.js
@@ -0,0 +1,30 @@
+//------ DrowpDown Mode Buttom-------
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) {
+  if (!event.target.matches("nav__dropdown-btn")) {
+    var nav__dropdown = document.getElementsByClassName(
+      "nav__dropdown-content"
+    );
+    var i;
+    for (i = 0; i < nav__dropdown.length; i++) {
+      var openDropdown = nav__dropdown[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+
+//------ Menu Item Active-------
+var btnContainer = document.getElementById("mylist");
+var btns = btnContainer.getElementsByClassName("nav__items");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
