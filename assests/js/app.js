101c676f7288a51576911228dc20870ae021fae2
zahra imani <zahra.imani753@gmail.com>
2024-06-07 22:29:57 +0330

    bedon dark mod

--- assests/js/app.js
+++ assests/js/app.js
@@ -12,8 +12,7 @@ const prioritSelectionBtn = document.querySelector(".priority__selection--btn");
 const prioritSelectionBtnIcon = document.querySelector(
   ".priority__selection--btn svg"
 );
-const crudBoxIcon = document.querySelector('.bx-dots-vertical-rounded');
-const crudBox = document.querySelector('.crud__box');
+
 const inComplateToDoListSection = document.querySelector(".incomplate");
 const errorMessage = document.querySelector('.error-message');
 
@@ -21,12 +20,6 @@ const errorMessage = document.querySelector('.error-message');
 toDoAddBtn.addEventListener("click", ToDOAddBtnHandler);
 toDoListTaskAdd.addEventListener("click", ToDoListTaskAddhandler);
 prioritSelectionBtn.addEventListener("click", PrioritSelectionBtnHandler);
-crudBoxIcon.addEventListener("click", CrudBoxIconHandler);
-
-function CrudBoxIconHandler(){
-  crudBox.classList.add("show");
-
-}
 
 function ToDoListTaskAddhandler() {
   toDoListTaskAdd.style.display = "none";
@@ -43,13 +36,13 @@ function PrioritSelectionBtnHandler() {
 function ToDOAddBtnHandler() {
   let selectedPriority;
 
-  // ارور میده خالی برنگردونه
+  // Validate the title input
   if (!toDoTitleUser.value.trim()) {
     errorMessage.style.display = "block";
     return;
   }
 
-  // مقدار اهمیتش رو پیدا میکنه
+  // Find the selected priority
   for (let i = 0; i < priorities.length; i++) {
     if (priorities[i].checked) {
       selectedPriority = priorities[i].value;
@@ -57,24 +50,24 @@ function ToDOAddBtnHandler() {
     }
   }
 
-  // ابجکت لیست رو میسازه
+  // Create the new task object
   const inCompleteTask = {
     title: toDoTitleUser.value,
     desc: toDoDescUser.value,
     priority: selectedPriority
   };
 
-  // ایتم هارو میگیره میبره لوکال
+  // Load the existing incomplete tasks from localStorage
   let inCompleteTasks = JSON.parse(localStorage.getItem('inCompleteTasks')) || [];
 
-  // برعکس به ارایه اضافه میکنه
+  // Add the new task to the list and save it to localStorage
   inCompleteTasks.unshift(inCompleteTask);
   localStorage.setItem('inCompleteTasks', JSON.stringify(inCompleteTasks));
 
-  // وقتی تسک جدید اضافه شد ریفرش میکنه صفحه رو
+  // Refresh the incomplete tasks list
   refreshIncompleteTasks();
 
-  // بعد اضافه کردن اینپوت رو خالی میکنه
+  // Clear the input fields and hide the add task box
   toDoTitleUser.value = '';
   toDoDescUser.value = '';
   toDoListTaskAddOpen.classList.remove("show");
@@ -84,13 +77,13 @@ function ToDOAddBtnHandler() {
 }
 
 function refreshIncompleteTasks() {
-  // لیست رو خالی میکنه
+  // Clear the existing incomplete tasks list
   inComplateToDoListSection.innerHTML = '';
 
   // Retrieve the incomplete tasks from localStorage
   let inCompleteTasks = JSON.parse(localStorage.getItem('inCompleteTasks')) || [];
 
-  // تسک هایی که کاربر زده رو اضافه میکنه
+  // Create and append the task cards to the list
   if (inCompleteTasks.length > 0) {
     inCompleteTasks.forEach((todo) => {
       const todoCard = document.createElement("div");
@@ -99,30 +92,10 @@ function refreshIncompleteTasks() {
         todoCard.style.borderRight ="2px solid #11a483";
         todoCard.innerHTML = `
         <div class="incomplate__item--top low">
-        <input type="checkbox" id="todocheckbox" checked=""/>
+        <input type="checkbox" id="todocheckbox" />
         <h2>${todo.title}</h2>
         <span class="low-priority tag"> ${todo.priority} </span>
         <i class="bx bx-dots-vertical-rounded"></i>
-        <div class="crud__box">
-            <span class="crud__box--selector">
-              <i class='bx bx-trash'></i>
-              <input
-                type="radio"
-                name="CRUD"
-                id="delete"
-                value="delete"
-              />
-            </span>
-            <span class="crud__box--selector">
-              <i class='bx bx-edit-alt'></i>
-              <input
-                type="radio"
-                name="CRUD"
-                id="edit"
-                value="edit"
-              />
-            </span>
-          </div>
       </div>
         <p>${todo.desc}</p>
       `;
@@ -131,30 +104,10 @@ function refreshIncompleteTasks() {
       todoCard.style.borderRight ="2px solid #ffaf37";
       todoCard.innerHTML = `
       <div class="incomplate__item--top mid">
-      <input type="checkbox" id="todocheckbox" checked=""/>
+      <input type="checkbox" id="todocheckbox"  />
       <h2>${todo.title}</h2>
       <span class="mid-priority tag"> ${todo.priority} </span>
       <i class="bx bx-dots-vertical-rounded"></i>
-      <div class="crud__box">
-            <span class="crud__box--selector">
-              <i class='bx bx-trash'></i>
-              <input
-                type="radio"
-                name="CRUD"
-                id="delete"
-                value="delete"
-              />
-            </span>
-            <span class="crud__box--selector">
-              <i class='bx bx-edit-alt'></i>
-              <input
-                type="radio"
-                name="CRUD"
-                id="edit"
-                value="edit"
-              />
-            </span>
-          </div>
     </div>
       <p>${todo.desc}</p>
       `;
@@ -163,30 +116,10 @@ function refreshIncompleteTasks() {
         todoCard.style.borderRight ="2px solid #ff5f37";
         todoCard.innerHTML = `
         <div class="incomplate__item--top high">
-        <input type="checkbox" id="todocheckbox" checked="" />
+        <input type="checkbox" id="todocheckbox"  />
         <h2>${todo.title}</h2>
         <span class="high-priority tag"> ${todo.priority} </span>
         <i class="bx bx-dots-vertical-rounded"></i>
-        <div class="crud__box">
-            <span class="crud__box--selector">
-              <i class='bx bx-trash'></i>
-              <input
-                type="radio"
-                name="CRUD"
-                id="delete"
-                value="delete"
-              />
-            </span>
-            <span class="crud__box--selector">
-              <i class='bx bx-edit-alt'></i>
-              <input
-                type="radio"
-                name="CRUD"
-                id="edit"
-                value="edit"
-              />
-            </span>
-          </div>
       </div>
         <p>${todo.desc}</p>
     `;}
@@ -194,12 +127,9 @@ function refreshIncompleteTasks() {
     });
     toDoListNoTaskImg.style.display = "none";
   } else {
-    // اگه تسکی نبود میاد عکس رو به جاش نشون میده
+    // If there are no incomplete tasks, show the "No Task" image
     toDoListNoTaskImg.style.display = "block";
   }
 }
 
 document.addEventListener("DOMContentLoaded", refreshIncompleteTasks);
-
-
-
